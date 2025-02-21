b3402e2cd19714c51597b56ab044d80fa1ac0136
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-02-21 16:16:36 +0200

    csv-tiedostot refaktoroitu omaan kansioon.

--- nlp.py
+++ nlp.py
@@ -3,8 +3,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 
-train_df = pd.read_csv("./train.csv", encoding="ISO-8859-1")
-test_df = pd.read_csv("./test.csv", encoding="ISO-8859-1")
+train_df = pd.read_csv("./data_material/train.csv", encoding="ISO-8859-1")
+test_df = pd.read_csv("./data_material/test.csv", encoding="ISO-8859-1")
 
 train_df.dropna(inplace=True)
 test_df.dropna(inplace=True)
